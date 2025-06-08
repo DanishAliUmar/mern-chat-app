71193af7ad139da39a7a699a7a7ca45b1e9cd52f
Danish Ali Umar <danishaliumar4@gmail.com>
2025-06-09 02:11:22 +0500

    create schema of application through mongoose

---
+++ backend/models/userModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const userModel = mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    pic: { type: String, required: true, default: "backend/data/fallbackpng.png" },
+}, {
+    timestamps: true,
+})
+
+const User = mongoose.model("User", userModel)
+module.exports = User
