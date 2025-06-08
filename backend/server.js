fdcc62e5e19e94fbe44df664b1d3e3fb138695e6
Danish Ali Umar <danishaliumar4@gmail.com>
2025-06-09 03:33:44 +0500

    create UI of app and setup MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const chats = require('./data/data');
+const connectDB = require('./config/db');
+const colors = require('colors');
+
 
 const app = express();
 dotenv.config();
+connectDB()
 app.get("/", (req, res) => {
     res.send("Api is Running on /")
 })
@@ -20,4 +24,4 @@ app.get("/api/chat/:id", (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log('Server is running on ' + PORT));
+app.listen(PORT, console.log(`Server is running on ${PORT}`.bgGreen.bold));
