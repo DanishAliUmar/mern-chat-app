f02639f107f34bc2361e1a8bf043c3d25395d6e9
Danish Ali Umar <danishaliumar4@gmail.com>
2025-06-15 02:08:55 +0500

    Working on Auth module

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 
 const userModel = mongoose.Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    pic: { type: String, required: true, default: "backend/data/fallbackpng.png" },
+    pic: { type: String, default: "backend/data/fallbackpng.png" },
 }, {
     timestamps: true,
 })
--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,18 @@ const dotenv = require('dotenv');
 const chats = require('./data/data');
 const connectDB = require('./config/db');
 const colors = require('colors');
+const userRoutes = require('./routes/userRoutes')
 
 
 const app = express();
 dotenv.config();
 connectDB()
 app.get("/", (req, res) => {
-    res.send("Api is Running on /")
-})
-
-app.get("/api/chat", (req, res) => {
-    res.send(chats)
+    res.send("Api is Running")
 })
 
 
-app.get("/api/chat/:id", (req, res) => {
-    const singleChat = chats.find((chat) => chat._id === req.params.id)
-    console.log(singleChat);
-    res.send(singleChat)
-})
+app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on ${PORT}`.bgGreen.bold));
