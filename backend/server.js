03a8b06bfe6164b58111549ab4da9e8a7100379d
Danish Ali Umar <danishaliumar4@gmail.com>
2025-06-15 23:54:52 +0500

    create notFound and errorHandler to manage Errors

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ const dotenv = require('dotenv');
 const chats = require('./data/data');
 const connectDB = require('./config/db');
 const colors = require('colors');
-const userRoutes = require('./routes/userRoutes')
+const userRoutes = require('./routes/userRoutes');
+const { errorHandler, notFound } = require('./middleware/errorMiddlewere');
 
 
 const app = express();
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
 dotenv.config();
 connectDB()
 app.get("/", (req, res) => {
@@ -16,6 +17,9 @@ app.get("/", (req, res) => {
 
 
 app.use('/api/user', userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server is running on ${PORT}`.bgGreen.bold));
