7c53a970027086441265806d85c645aed3487b02
Danish Ali Umar <danishaliumar4@gmail.com>
2025-07-21 09:35:21 +0500

    Fix Rename Api schema Issue

--- backend/models/chatModel.js
+++ backend/models/chatModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const chatModel = mongoose.Schema({
-    chatNmae: { type: String, trim: true },
+    chatName: { type: String, trim: true },
     isGroupChat: { type: Boolean, default: false },
     users: [
         {
