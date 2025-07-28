ed46bc4ef0884679cf9f4170c33d744b3c1b548d
Danish Ali Umar <danishaliumar4@gmail.com>
2025-07-28 13:26:52 +0500

    fix chat Route issue

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,11 @@ const userModel = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    pic: { type: String, default: "backend/data/fallbackpng.png" },
+    pic: {
+        type: String,
+        default:
+            "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
 }, {
     timestamps: true,
 })
